27528dc4e4a4039051d445d26e531b1c71dd9526
Diviloper <vdiviloper@gmail.com>
2024-03-30 21:51:47 +0100

    Add arithmetic.c

---
+++ meos/examples/arithmetic.c
@@ -0,0 +1,23 @@
+//
+// Created by Victor on 30/3/2024.
+//
+
+
+#include <stdio.h>
+#include <meos.h>
+
+/* Main program */
+int main(void) {
+    meos_initialize(NULL, NULL);
+
+    Temporal *tfloat = tfloat_in("[1@2020-03-01, 10@2020-03-10]");
+    Temporal *div = div_tfloat_float(tfloat, 2.0);
+
+    printf("Original: %s\n", tfloat_out(tfloat, 3));
+    printf("Divided by 2: %s\n", tfloat_out(div, 3));
+
+    meos_finalize();
+
+    return 0;
+}
+
